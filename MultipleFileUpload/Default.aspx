55b9853758dda4f3c1f27887cc89a829299b581e
RODRIGO OLIVEIRA <rodrigo.oliveira85@fatec.sp.gov.br>
2021-07-14 19:45:16 -0300

    first commit

---
+++ MultipleFileUpload/Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+   <title>Multiple File Upload</title>
+   
+</head>
+<body>
+     <form id="form1" runat="server">   
+        <div id="divFile">
+            <h3>Multiple File Upload in Asp.Net (C#)</h3>  
+            
+            <p>
+                <asp:FileUpload ID="fileUpload" multiple="true" runat="server" />   
+            </p>
+            <p>
+                <asp:Button ID="btUpload" Text ="Upload Files" 
+                    OnClick="Upload_Files" runat="server" />
+            </p>
+
+            <%--SHOW UPLOAD MESSAGE--%>
+            <p><asp:label id="lblFileList" runat="server"></asp:label></p>
+            <p><asp:Label ID="lblUploadStatus" runat="server"></asp:Label></p>
+            <p><asp:Label ID="lblFailedStatus" runat="server"></asp:Label></p>
+        </div>
+    </form>
+</body>
+    <script>
+        $('#btUpload').click(function() { 
+            if (fileUpload.value.length == 0) {    // CHECK IF FILE(S) SELECTED.
+                alert('No files selected.');        
+                return false; 
+            } 
+        });
+    </script>
+
+
+</html>
---
+++ MultipleFileUpload/Default.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+    protected void Upload_Files(object sender, EventArgs e)
+    {
+        if (fileUpload.HasFile)     // CHECK IF ANY FILE HAS BEEN SELECTED.
+        {
+            int iUploadedCnt = 0;
+            int iFailedCnt = 0;
+            HttpFileCollection hfc = Request.Files;
+            lblFileList.Text = "Select <b>" + hfc.Count + "</b> file(s)";
+
+            if (hfc.Count <= 10)    // 10 FILES RESTRICTION.
+            {
+                for (int i = 0; i <= hfc.Count - 1; i++)
+                {
+                    HttpPostedFile hpf = hfc[i];
+                    if (hpf.ContentLength > 0)
+                    {
+                        if (!File.Exists(Server.MapPath("CopyFiles\\") +
+                            Path.GetFileName(hpf.FileName)))
+                        {
+                            DirectoryInfo objDir =
+                                new DirectoryInfo(Server.MapPath("CopyFiles\\"));
+
+                            string sFileName = Path.GetFileName(hpf.FileName);
+                            string sFileExt = Path.GetExtension(hpf.FileName);
+
+                            // CHECK FOR DUPLICATE FILES.
+                            FileInfo[] objFI =
+                                objDir.GetFiles(sFileName.Replace(sFileExt, "") + ".*");
+
+                            if (objFI.Length > 0)
+                            {
+                                // CHECK IF FILE WITH THE SAME NAME EXISTS  (IGNORING THE EXTENTIONS).
+                                foreach (FileInfo file in objFI)
+                                {
+                                    string sFileName1 = objFI[0].Name;
+                                    string sFileExt1 = Path.GetExtension(objFI[0].Name);
+
+                                    if (sFileName1.Replace(sFileExt1, "") ==
+                                            sFileName.Replace(sFileExt, ""))
+                                    {
+                                        iFailedCnt += 1;        // NOT ALLOWING DUPLICATE.
+                                        break;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                // SAVE THE FILE IN A FOLDER.
+                                hpf.SaveAs(Server.MapPath("CopyFiles\\") +
+                                    Path.GetFileName(hpf.FileName));
+                                iUploadedCnt += 1;
+                            }
+                            }
+                        }
+                    }
+                    lblUploadStatus.Text = "<b>" + iUploadedCnt + "</b> file(s) Uploaded.";
+                                    lblFailedStatus.Text = "<b>" + iFailedCnt + 
+                                        "</b> duplicate file(s) could not be uploaded.";
+            }
+            else lblUploadStatus.Text = "Max. 10 files allowed.";
+        }
+        else lblUploadStatus.Text = "No files selected.";
+    }
+
+
+
+
+
+
+}
+
